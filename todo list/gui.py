b940a700c5cab34dbd55f1ce38842063f27829f4
DanTheMaN-231 <dwhkotze12@gmail.com>
2025-03-04 22:43:39 +0200

    Add Edit button along with todos

--- todo list/gui.py
+++ todo list/gui.py
@@ -4,10 +4,13 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.open_file(), key='todos', 
+                      enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App", 
                     layout=[[label],
-                    [input_box , add_button]] ,
+                    [input_box , add_button],[list_box, edit_button]],
                     font=('Helvetica', 15))
 
 while True:
@@ -21,4 +24,5 @@ while True:
         functions.write_data(todos)
     elif event == sg.WIN_CLOSED:
         break
+
 window.close()
