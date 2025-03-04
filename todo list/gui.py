b1990252773b7bd5990ddc9e60aa37c617b9607b
DanTheMaN-231 <dwhkotze12@gmail.com>
2025-03-04 12:50:11 +0200

    Add_basic gui
    I added a basic gui without functionality by importing the FreeSimpleGui
    library.

--- todo list/gui.py
+++ todo list/gui.py
@@ -1,6 +1,10 @@
 import functions # pip install FreeSimpleGUI
 import FreeSimpleGUI as sg
 
-window = sg.Window("My To-Do App", layout=[""])
+label = sg.Text("Type in a To-Do")
+input_box = sg.InputText("Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My To-Do App", layout=[[label],[input_box , add_button]])
 window.read()
 window.close()
